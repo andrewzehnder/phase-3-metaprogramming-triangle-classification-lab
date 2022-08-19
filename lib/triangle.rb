afa4d336e3db547e197b7aa4ce5d3e9027a6287c
Andrew <azehnder2@gmail.com>
2022-08-19 15:05:15 -0700

    commit tree bc6ee7fc652b19b901f0be6b06f96903c86623ad

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_accessor :one, :two, :three, :kind
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+  
+  def kind
+    # sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+    #Each side must be larger than 0
+    if (@one + @two) <= @three || (@two + @three) <= @one || (@three + @one) <= @two
+      raise TriangleError
+
+    elsif @one == @two && @one == @three
+      self.kind = :equilateral
+
+    elsif @one == @two || @one == @three || @two == @three
+      self.kind = :isosceles
+
+    else
+      self.kind = :scalene
+
+    end
+
+  end
+
+  class TriangleError < StandardError
+    #triangle error code
+  end
+
 end
